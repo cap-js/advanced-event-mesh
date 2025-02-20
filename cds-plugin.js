7bfcf05076ef01a80857717d2b16de805f2edcc6
Dr. David A. Kunz <david.kunz@sap.com>
2025-02-20 11:41:42 +0100

    .

--- cds-plugin.js
+++ cds-plugin.js
@@ -33,6 +33,31 @@ const normalizeIncomingMessage = message => {
   }
 }
 
+const getAppMetadata = () => {
+  // NOT official, but consistent with Event Mesh!
+  const appMetadata = cds.env.app
+
+  if (appMetadata) {
+    return {
+      appID: appMetadata.id,
+      appName: appMetadata.name,
+      appURL: appMetadata.url
+    }
+  }
+
+  const vcapApplication = process.env.VCAP_APPLICATION && JSON.parse(process.env.VCAP_APPLICATION)
+
+  return {
+    appID: vcapApplication && vcapApplication.application_id,
+    appName: vcapApplication && vcapApplication.application_name,
+    appURL:
+      vcapApplication &&
+      vcapApplication.application_uris &&
+      vcapApplication.application_uris[0] &&
+      `https://${vcapApplication.application_uris[0].replace(/^https?:\/\//, '')}`
+  }
+}
+
 module.exports = class AdvancedEventMesh extends cds.MessagingService {
   async init() {
     await super.init()
@@ -55,7 +80,7 @@ module.exports = class AdvancedEventMesh extends cds.MessagingService {
       this.startListening()
     })
 
-    const optionsApp = require('@sap/cds/libx/_runtime/common/utils/vcap.js') // TODO: streamline
+    const optionsApp = getAppMetadata()
     const appId = () => {
       const appName = optionsApp.appName || 'CAP'
       const appID = optionsApp.appID || '00000000'
